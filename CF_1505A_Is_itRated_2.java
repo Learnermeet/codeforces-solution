48a5f5034b1bbd247a658931aa0948e85953d6b7
Learnermeet <parmarmeet603@gmail.com>
2026-04-03 14:01:22 +0530

    Create CF_1505A_Is_itRated_2.java

---
+++ CF_1505A_Is_itRated_2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CF_1505A_Is_itRated_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            sc.nextLine();   
+            System.out.println("NO");
+        }
+    }
+}
