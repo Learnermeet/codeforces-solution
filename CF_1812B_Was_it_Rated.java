3f6d344abc1c485536872d4b2cd269fc7a2e889a
Learnermeet <parmarmeet603@gmail.com>
2026-04-03 17:14:51 +0530

    Create CF_1812B_Was_it_Rated.java

---
+++ CF_1812B_Was_it_Rated.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class CF_1812B_Was_it_Rated {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n == 15 || n == 20 || n == 21){
+            System.out.println("NO");
+        }
+        else{
+            System.out.println("YES");
+        }
+    }
+}
