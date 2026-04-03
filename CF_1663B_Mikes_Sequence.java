136f1bc23159db7f118fba58fef7008618109e5a
Learnermeet <parmarmeet603@gmail.com>
2026-04-03 14:02:55 +0530

    Create CF_1663B_Mikes_Sequence.java

---
+++ CF_1663B_Mikes_Sequence.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class CF_1663B_Mikes_Sequence { 
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if (n < 1200) {
+            System.out.println(1200);
+        } else if (n < 1400) {
+            System.out.println(1400);
+        } else if (n < 1600) {
+            System.out.println(1600);
+        } else if (n < 1900) {
+            System.out.println(1900);
+        } else if (n < 2100) {
+            System.out.println(2100);
+        } else if (n < 2300) {
+            System.out.println(2300);
+        } else if (n < 2400) {
+            System.out.println(2400);
+        } else if (n < 3000) {
+            System.out.println(3000);
+        }
+    }
+}
