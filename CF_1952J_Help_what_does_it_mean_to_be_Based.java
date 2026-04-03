1166b4d6904aca01ce48cd9c76e4c20c815dae00
Learnermeet <parmarmeet603@gmail.com>
2026-04-03 17:48:23 +0530

    Create CF_1952J_Help_what_does_it_mean_to_be_Based.java

---
+++ CF_1952J_Help_what_does_it_mean_to_be_Based.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class CF_1952J_Help_what_does_it_mean_to_be_Based {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x == 4) {
+            System.out.println("yoink n\n" +
+                    "yoink a\n" +
+                    "yoink k\n" +
+                    "bruh s is lowkey just -1\n" +
+                    "*slaps s on top of k*\n" +
+                    "bruh i is lowkey just 0\n" +
+                    "vibe check n ratios i\n" +
+                    "simp for 11\n" +
+                    "yeet a[k]\n" +
+                    "go touch some grass\n" +
+                    "bruh m is lowkey just a[i]\n" +
+                    "bruh L is lowkey just i\n" +
+                    "bruh j is lowkey just i\n" +
+                    "vibe check n ratios j\n" +
+                    "simp for 21\n" +
+                    "bruh temp is lowkey just a[L]\n" +
+                    "bruh a[L] is lowkey just a[i]\n" +
+                    "bruh a[i] is lowkey just temp\n" +
+                    "*slaps 1 on top of i*\n" +
+                    "simp for 7\n" +
+                    "vibe check a[j] ratios m\n" +
+                    "bruh L is lowkey just j\n" +
+                    "vibe check a[j] ratios m\n" +
+                    "bruh m is lowkey just a[j]\n" +
+                    "*slaps 1 on top of j*\n" +
+                    "simp for 14");
+        }
+        else if (x == 3) {
+            System.out.println("yoink n\n" +
+                    "yoink a\n" +
+                    "bruh m is lowkey just a[0]\n" +
+                    "bruh i is lowkey just 1\n" +
+                    "vibe check n ratios i\n" +
+                    "simp for 9\n" +
+                    "yeet m\n" +
+                    "go touch some grass\n" +
+                    "vibe check a[i] ratios m\n" +
+                    "bruh m is lowkey just a[i]\n" +
+                    "*slaps 1 on top of i*\n" +
+                    "simp for 5");
+        }
+        else if (x == 2) {
+            System.out.println("yoink a\n" +
+                    "bruh b is lowkey just 0\n" +
+                    "rip this b fell off by a\n" +
+                    "vibe check a ratios b\n" +
+                    "simp for 7\n" +
+                    "bruh a is lowkey just b\n" +
+                    "yeet a\n" +
+                    "go touch some grass\n");
+        }
+        else {
+            System.out.println("yoink a\n" +
+                    "yoink b\n" +
+                    "*slaps a on top of b*\n" +
+                    "yeet b\n" +
+                    "go touch some grass");
+        }
+    }
+}
