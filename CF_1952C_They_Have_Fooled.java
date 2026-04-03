6a38fc07b1084f90fea83c4b40413f3bd1dc52d4
Learnermeet <parmarmeet603@gmail.com>
2026-04-03 17:38:02 +0530

    Create CF_1952C_They_Have_Fooled.java

---
+++ CF_1952C_They_Have_Fooled.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CF_1952C_They_Have_Fooled {
+    public static void main(String[] args) {
+        int s = new Scanner(System.in).nextInt();
+        switch (s) {
+            case 0 -> System.out.println(10);
+            case 1 -> System.out.println(10);
+            case 2 -> System.out.println(8);
+            case 3 -> System.out.println(9);
+            case 4 -> System.out.println(8);
+            case 5 -> System.out.println(7);
+            case 6 -> System.out.println(7);
+            case 7 -> System.out.println(7);
+            case 8 -> System.out.println(7);
+            case 9 -> System.out.println(0);
+            case 10 -> System.out.println(9);
+            case 11 -> System.out.println(6);
+            case 12 -> System.out.println(8);
+        }
+    }
+}
