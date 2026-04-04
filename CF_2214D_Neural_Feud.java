9bb1ba9801c24d86e9740cd740349ded17ffc6d4
Learnermeet <parmarmeet603@gmail.com>
2026-04-04 15:09:41 +0530

    Create CF_2214D_Neural_Feud.java

---
+++ CF_2214D_Neural_Feud.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CF_2214D_Neural_Feud {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        switch (n) {
+            case 1: System.out.println("walk"); break;
+            case 2: System.out.println("no"); break;
+            case 3: System.out.println("no"); break;
+            case 4: System.out.println("no"); break;
+            case 5: System.out.println("yes"); break;
+            case 6: System.out.println("yes"); break;
+            case 7: System.out.println("backwards"); break;
+            case 8: System.out.println("seven"); break;
+        }
+    }
+}
