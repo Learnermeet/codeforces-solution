08f61b0951d1c598fda2afef7427dfa519e6f66c
Learnermeet <parmarmeet603@gmail.com>
2026-04-04 15:08:09 +0530

    Create CF_2214B_Are_you_smilng.java

---
+++ CF_2214B_Are_you_smilng.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CF_2214B_Are_you_smilng {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            sc.next();
+        }
+        System.out.println("1F601");
+    }
+}
